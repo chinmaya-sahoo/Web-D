5644a591a13edb43d3aa35bbf857a0caab19b6fe
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-04 20:50:09 +0530

    c

--- V81Chal-14Soln/script.js
+++ V81Chal-14Soln/script.js
@@ -1,22 +1,29 @@
-const randomDelay = () =>{
-    return new Promise((resolve , reject) =>{
+setInterval(() => {
+   let last = document.body.lastElementChild
+}, 700)
+
+const addition = async (item) => {
+    await randomDelay();
+    let div = document.createElement("div");
+    div.innerHTML = item;
+    document.body.append(div)
+}
+
+const randomDelay = () => {
+    return new Promise((resolve, reject) => {
         timeout = 1 + 6 * Math.random()
-        setTimeout(()=>{
-           resolve() 
-        },timeout*1000)
+        setTimeout(() => {
+            resolve()
+        }, timeout * 1000)
     })
 }
 
-let text = ["Initializing Hacking...",
-    "Reading Files...",
-    "Password File Detected...",
-    "Sending all password and personal files to server...",
-    "Cleaning up..."]
+let text = ["Initializing Hacking",
+    "Reading Files",
+    "Password File Detected",
+    "Sending all password and personal files to server",
+    "Cleaning up"]
 
-    for(const item of text){
-        addition(item)
-    }
-
-    const addition = async(item)=>{
-        await randomDelay();
-    }
+for (const item of text) {
+    addition(item)
+}
