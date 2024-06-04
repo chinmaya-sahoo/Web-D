56f6b123a5e38b591c30ec1f069146c2f2764c9f
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-04 21:11:05 +0530

    c

--- V81Chal-14Soln/script.js
+++ V81Chal-14Soln/script.js
@@ -1,6 +1,3 @@
-setInterval(() => {
-   let last = document.body.lastElementChild
-}, 700)
 
 const addition = async (item) => {
     await randomDelay();
@@ -18,12 +15,30 @@ const randomDelay = () => {
     })
 }
 
-let text = ["Initializing Hacking",
+async function main(){
+    
+    
+    let t = setInterval(() => {
+        let last = document.body.getElementsByTagName("div");
+        last = last[last.length-1]
+        if(last.innerHTML.endsWith("...")){
+            last.innerHTML = last.innerHTML.slice(0,last.innerHTML.length-3)
+        }
+        last.innerHTML = last.innerHTML + "."
+    }, 500)
+    
+    let text = ["Initializing Hacking",
     "Reading Files",
     "Password File Detected",
     "Sending all password and personal files to server",
     "Cleaning up"]
 
-for (const item of text) {
-    addition(item)
+    for (const item of text) {
+        await addition(item)
+    }
+    
+    await randomDelay()
+    clearInterval(t)
 }
+
+main()
