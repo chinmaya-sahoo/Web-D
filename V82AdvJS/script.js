6a0bb5d8f12ef8e1ed8c8420d51e3bbc00e1e165
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-05 11:56:18 +0530

    C

--- V82AdvJS/script.js
+++ V82AdvJS/script.js
@@ -2,17 +2,25 @@ async function sleep() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(45)
-        },1000);
+        }, 1000);
     })
 }
 
-(async function main(){
-    
+(async function main() {
+
     // let a = await sleep()
     // console.log(a)
     // let b = await sleep()
     // console.log(b)
 
-    let [x , y, ...rest] = [1 ,5 , 7]
-    console.log(x , y, rest)
+    // let [x, y, ...rest] = [1, 5, 7, 8, 0, 9]
+    // console.log(x, y, rest)
+
+    let obj = {
+        a:1,
+        b:2,
+        c:3
+    }
+    let {a,b} = obj
+    console.log(a,b)
 })()
