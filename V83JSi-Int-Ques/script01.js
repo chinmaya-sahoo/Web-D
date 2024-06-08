eae99988b9b760e8b72b02f6b186b4bc596c914c
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-08 12:35:10 +0530

    n

--- V83JSi-Int-Ques/script01.js
+++ V83JSi-Int-Ques/script01.js
@@ -7,3 +7,22 @@
 // house alert question
 
 let students = ["aditya","chinmaya","sesma","ishi","priyaranjanpatra","priyalinita","sanmsleshita","pusparanjan"]
+
+let houses = []
+
+for(const student of students){
+    if(student.length<6){
+        houses.push("Griffindoor");
+    }
+    else if(student.length<8){
+        houses.push("Huffule");
+    }
+    else if(student.length<12){
+        houses.push("Gorbutcher");
+    }
+    else{
+        houses.push("sterlin")
+    }
+}
+
+console.log(houses)
