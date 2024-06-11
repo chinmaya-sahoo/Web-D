057c96f2b7b8cc87424f9d7c74b1745e38bcbf6b
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-11 08:25:12 +0530

    c

--- ALL-PROJECTS/solar system/script.js
+++ ALL-PROJECTS/solar system/script.js
@@ -3,8 +3,8 @@ function creatStars(){
     for(let i =0 ; i<1000 ;i++){
         const star = document.createElement("div");
         star.className = "star";
-        star.style.width = ".1px;"
-        star.style.height = ".1px;"
+        star.style.width = "0.1px;"
+        star.style.height = "0.1px;"
 
         star.style.top = Math.random()*100 + "%" ;
         star.style.left = Math.random()*100 + "%" ;
