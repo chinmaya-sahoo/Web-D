ae680646c580a6351f1fa5b0288c785bdf2fa085
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-07-03 20:32:02 +0530

    files added

---
+++ JS PRACTICE/P1ColorChanger/script.js
@@ -0,0 +1,20 @@
+let button1 = document.getElementById("grey")
+button1.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "grey" 
+})
+
+let button2 = document.getElementById("white")
+button2.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "white" 
+})
+
+let button3 = document.getElementById("blue")
+button3.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "blue" 
+})
+
+let button4 = document.getElementById("yellow")
+button4.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "yellow" 
+})
+
