bf56714391a2c44cff3440154dfde040198bb88d
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-07-05 22:10:34 +0530

    files added

---
+++ JS PRACTICE/P2-BMI-Generator/script.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("btn")
+button.addEventListener("click",()=>{
+    document.getElementById("results").innerHTML = "under weight"
+})
+
+let button1 = document.getElementById("grey")
+button1.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "grey" 
+})
