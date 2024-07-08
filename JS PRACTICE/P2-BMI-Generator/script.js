af56c421e9c6f3b45f250e04e716306cbce8f40e
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-07-08 19:19:07 +0530

    files added

--- JS PRACTICE/P2-BMI-Generator/script.js
+++ JS PRACTICE/P2-BMI-Generator/script.js
@@ -1,4 +1,19 @@
 let button = document.getElementById("btn")
-button.addEventListener("click",()=>{
-    document.getElementById("results").innerHTML = "under weight"
-})
+let height = parseInt(document.querySelector('#height'))
+let weight = parseInt(document.querySelector('#weight'))
+let BMI = 
+if(BMI<18.6){
+    button.addEventListener("click",()=>{
+        document.getElementById("results").innerHTML = "under weight"
+    })
+}
+else if(BMI>18.6 && BMI<24.9){
+    button.addEventListener("click",()=>{
+        document.getElementById("results").innerHTML = "under weight"
+    })
+}
+else{
+    button.addEventListener("click",()=>{
+        document.getElementById("results").innerHTML = "under weight"
+    })
+}
